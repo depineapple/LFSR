8b2c072a826af1d5b616e39299e83bb20e6d0b85
Никита Коваль <nikita.koval.01@bk.ru>
2022-03-27 22:10:52 +1000

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(284, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Зашифровать";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(284, 168);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Расшифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(12, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(201, 26);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(12, 133);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(201, 26);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(9, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Исходное сообщение";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(9, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Вектор коэффициентов";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(9, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Вектор инициализации";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(9, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Ключевой поток";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(12, 184);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(201, 26);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(9, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Шифротекст";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox5.Location = new System.Drawing.Point(12, 236);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(201, 26);
+            this.textBox5.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(284, 222);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 31);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Очистка";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(238, 52);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(277, 24);
+            this.radioButton1.TabIndex = 16;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Строковая последовательность";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(238, 75);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(260, 24);
+            this.radioButton2.TabIndex = 17;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Битовая последовательность";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 293);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text;
+using System.Collections;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private BitArray z, iv, c, m, r;
+        bool text = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1 == null || textBox2 == null || textBox3 == null)
+            { MessageBox.Show("Не введены исходные данные", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+            if (radioButton1.Checked == true)
+            {
+                m = ToBites(textBox1);
+                c = ToBites(textBox2);
+                iv = ToBites(textBox3);
+                z = ToBites(textBox3);
+            }
+            else if (radioButton1.Checked == true)
+            {
+                text = false;
+                char[] c = textBox1.Text.ToCharArray();
+                char[] c1 = textBox2.Text.ToCharArray();
+                char[] c2 = textBox3.Text.ToCharArray();
+                m = new BitArray(c.Length);
+                this.c = new BitArray(c1.Length);
+                iv = new BitArray(c2.Length);
+                z = new BitArray(c2.Length);
+                for (int i = 0; i < m.Length; i++)
+                {
+                    if (c[i] == '1')
+                        m[i] = true;
+                    else m[i] = false;
+                }
+                for (int i = 0; i < this.c.Length; i++)
+                {
+                    if (c1[i] == '1')
+                        this.c[i] = true;
+                    else this.c[i] = false;
+                }
+                for (int i = 0; i < iv.Length; i++)
+                {
+                    if (c2[i] == '1')
+                    { iv[i] = true; z[i] = true; }
+                    else { iv[i] = false; z[i] = false; }
+                }
+            }
+            while (z.Length != m.Length)
+                Action(iv, c);
+            for (int i = 0; i < z.Length; i++)
+                textBox4.Text = string.Concat(textBox4.Text, z[i] ? "1" : "0");
+            r = z.Xor(m);
+            string res1 = ToString(r);
+            textBox5.Text = res1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox5 == null || textBox2 == null || textBox3 == null)
+            { MessageBox.Show("Не введены исходные данные", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+            if (radioButton1.Checked == true)
+            {
+                r = ToBites(textBox5);
+                c = ToBites(textBox2);
+                iv = ToBites(textBox3);
+                z = ToBites(textBox3);
+            }
+            else if (radioButton1.Checked == true)
+            {
+                text = false;
+                char[] c = textBox5.Text.ToCharArray();
+                char[] c1 = textBox2.Text.ToCharArray();
+                char[] c2 = textBox3.Text.ToCharArray();
+                r = new BitArray(c.Length);
+                this.c = new BitArray(c1.Length);
+                iv = new BitArray(c2.Length);
+                z = new BitArray(c2.Length);
+                for (int i = 0; i < r.Length; i++)
+                {
+                    if (c[i] == '1')
+                        r[i] = true;
+                    else r[i] = false;
+                }
+                for (int i = 0; i < this.c.Length; i++)
+                {
+                    if (c1[i] == '1')
+                        this.c[i] = true;
+                    else this.c[i] = false;
+                }
+                for (int i = 0; i < iv.Length; i++)
+                {
+                    if (c2[i] == '1')
+                    { iv[i] = true; z[i] = true; }
+                    else { iv[i] = false; z[i] = false; }
+                }
+            }
+            while (z.Length != r.Length)
+                Action(iv, c);
+            for (int i = 0; i < z.Length; i++)
+                textBox4.Text = string.Concat(textBox4.Text, z[i] ? "1" : "0");
+            m = z.Xor(r);
+            string res1 = ToString(m);
+            textBox1.Text = res1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = ""; textBox2.Text = ""; textBox3.Text = ""; textBox4.Text = ""; textBox5.Text = ""; text = true;
+        }
+
+        BitArray ToBites(TextBox textBox)
+        {
+            byte[] b = Encoding.UTF8.GetBytes(textBox.Text); BitArray bitArray = new BitArray(b);
+            return bitArray;
+        }
+
+        string ToString(BitArray bitArray)
+        {
+            string result = "";
+            //bitArray.ToString();
+            if (text == true)
+            {
+                byte[] b = BitArrayToBytes(bitArray);
+                result = Encoding.UTF8.GetString(b);
+            }
+            else
+            {
+                for (int i = 0; i < bitArray.Length; i++)
+                    result = string.Concat(result, bitArray[i] ? "1" : "0");
+            }
+            return result;
+        }
+
+        public static byte[] BitArrayToBytes(BitArray bitarray)
+        {
+            if (bitarray.Length == 0)
+            {
+                throw new ArgumentException("must have at least length 1", "bitarray");
+            }
+
+            int num_bytes = bitarray.Length / 8;
+
+            if (bitarray.Length % 8 != 0)
+            {
+                num_bytes += 1;
+            }
+
+            var bytes = new byte[num_bytes];
+            bitarray.CopyTo(bytes, 0);
+            return bytes;
+        }
+
+        void Action(BitArray bitArray1, BitArray bitArray2)
+        {
+            BitArray res = bitArray1.And(bitArray2);
+            bool r = res[0];
+            for (int i = 1; i < res.Length; i++)
+                r = r ^ res[i];
+            z.Length = z.Length + 1;
+            z.Set(z.Length - 1, r);
+            int len = z.Length;
+            bool[] gamma_new = new bool[len];
+            z.CopyTo(gamma_new, 0);
+            bool[] beta_new = new bool[c.Length];
+            Array.Copy(gamma_new, z.Length - c.Length, beta_new, 0, c.Length);
+            iv = new BitArray(beta_new);
+        }
+    }
+}
