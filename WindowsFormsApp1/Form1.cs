78b901c6b875e37d41b61daee841c39aaec3b766
Никита Коваль <nikita.koval.01@bk.ru>
2022-04-11 21:11:28 +1000

    Вынес логику в отдельный метод

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -23,71 +23,72 @@ namespace WindowsFormsApp1
 
         private void Encrypt_Click(object sender, EventArgs e)
         {
-            if (textBoxMessage == null || textBoxСoefficientVector == null || textBoxInitializationVector == null)
-            {
-                MessageBox.Show("Не введены исходные данные", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return;
-            }
-
-            if (stringSequence.Checked)
-                CaseStringSequence();
-
-            else if (bitSequence.Checked)
-                CaseBitSequence();
-
-            while (keyStream.Length != message.Length)
-                BitCalculations(initializationVector, coefficientVector);
-
-            for (int i = 0; i < keyStream.Length; i++)
-                textBoxKeyStream.Text = string.Concat(textBoxKeyStream.Text, keyStream[i] ? "1" : "0");
-
-            result = keyStream.Xor(message);
-            stringResult = parseUtils.BitArrayToString(result);
-            textBoxCipherText.Text = stringResult;
+            AlgorithmicPart(textBoxMessage, textBoxCipherText, message, result, 0);
         }
 
         private void Decrypt_Click(object sender, EventArgs e)
         {
-            if (textBoxCipherText == null || textBoxСoefficientVector == null || textBoxInitializationVector == null)
+            AlgorithmicPart(textBoxCipherText, textBoxMessage, result, message, 1);
+        }
+
+        private void IsNullInputData(TextBox textBox)
+        {
+            if (textBox.Text == "" || textBoxСoefficientVector.Text == "" || textBoxInitializationVector.Text == "")
             {
                 MessageBox.Show("Не введены исходные данные", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-
+        }
+        private void AlgorithmicPart(TextBox textBoxIn, TextBox textBoxOut, BitArray bitArrayIn, BitArray bitArrayOut, int operationType)
+        {
+            IsNullInputData(textBoxIn);
             if (stringSequence.Checked)
-                CaseStringSequence();
-
+                bitArrayIn = CaseStringSequence(bitArrayIn, operationType);
             else if (bitSequence.Checked)
-                CaseBitSequence();
-
-            while (keyStream.Length != result.Length)
+                bitArrayIn = CaseBitSequence(bitArrayIn, operationType);
+            while (keyStream.Length != bitArrayIn.Length)
                 BitCalculations(initializationVector, coefficientVector);
-
             for (int i = 0; i < keyStream.Length; i++)
                 textBoxKeyStream.Text = string.Concat(textBoxKeyStream.Text, keyStream[i] ? "1" : "0");
-
-            message = keyStream.Xor(result);
-            stringResult = parseUtils.BitArrayToString(message);
-            textBoxMessage.Text = stringResult;
+            bitArrayOut = keyStream.Xor(bitArrayIn);
+            stringResult = parseUtils.BitArrayToString(bitArrayOut);
+            textBoxOut.Text = stringResult;
         }
-
-        private void CaseStringSequence()
+        private BitArray CaseStringSequence(BitArray bitArray, int operationType)
         {
-            message = parseUtils.TextBoxStringToBites(textBoxMessage);
-            result = parseUtils.TextBoxStringToBites(textBoxCipherText);
             coefficientVector = parseUtils.TextBoxStringToBites(textBoxСoefficientVector);
             initializationVector = parseUtils.TextBoxStringToBites(textBoxInitializationVector);
             keyStream = parseUtils.TextBoxStringToBites(textBoxInitializationVector);
+            if (operationType == 0)
+            {
+                message = parseUtils.TextBoxStringToBites(textBoxMessage);
+                bitArray = message;
+            }
+            else if (operationType == 1)
+            {
+                result = parseUtils.TextBoxStringToBites(textBoxCipherText);
+                bitArray = result;
+            }
+            return bitArray;
         }
 
-        private void CaseBitSequence()
+        private BitArray CaseBitSequence(BitArray bitArray, int operationType)
         {
             isText = false;
-            message = parseUtils.CharSymbolToBoolSymbol(message, textBoxMessage);
-            result = parseUtils.CharSymbolToBoolSymbol(result, textBoxCipherText);
             coefficientVector = parseUtils.CharSymbolToBoolSymbol(coefficientVector, textBoxСoefficientVector);
             initializationVector = parseUtils.CharSymbolToBoolSymbol(initializationVector, textBoxInitializationVector);
             keyStream = parseUtils.CharSymbolToBoolSymbol(keyStream, textBoxInitializationVector);
+            if (operationType == 0)
+            {
+                message = parseUtils.CharSymbolToBoolSymbol(message, textBoxMessage);
+                bitArray = message;
+            }
+            else if (operationType == 1)
+            {
+                result = parseUtils.CharSymbolToBoolSymbol(result, textBoxCipherText);
+                bitArray = result;
+            }
+            return bitArray;
         }
         private void Clear_Click(object sender, EventArgs e)
         {
